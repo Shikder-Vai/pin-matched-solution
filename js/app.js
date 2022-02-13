58dd8008149b9723fd6b308e6765749f9b01c88d
unknown <Shikder-Vai>
2022-02-14 02:08:32 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+function getPin() {
+  const pin = Math.round(Math.random() * 10000);
+  const pinString = pin + "";
+  if (pinString.length == 4) {
+    return pin;
+  } else {
+    return getPin();
+  }
+}
+
+function generatePin() {
+  const pin = getPin();
+  //Methods 1
+  //   const generatedPin = document.getElementById("show-pin");
+  //   generatedPin.value = pin;
+  //Method 2
+  document.getElementById("show-pin").value = pin;
+}
+//calculator
+document.getElementById("key-pad").addEventListener("click", function (event) {
+  const calcInput = event.target.innerText;
+  const displayKey = document.getElementById("display-key");
+  if (isNaN(calcInput)) {
+    if (calcInput == "C") {
+      displayKey.value = "";
+    }
+  } else {
+    const previusNumber = displayKey.value;
+    const currentNumber = previusNumber + calcInput;
+    displayKey.value = currentNumber;
+  }
+});
+
+//Pin Matching
+function varifyPin() {
+  const displayKey = document.getElementById("display-key");
+  const inputField = displayKey.value;
+  const showPin = document.getElementById("show-pin").value;
+  const successed = document.getElementById("success-massage");
+  const faild = document.getElementById("fail-massage");
+  if (showPin == inputField) {
+    successed.style.display = "block";
+    faild.style.display = "none";
+    displayKey.value = "";
+  } else {
+    faild.style.display = "block";
+    successed.style.display = "none";
+  }
+}
